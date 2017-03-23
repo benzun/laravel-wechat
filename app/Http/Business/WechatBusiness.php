b4b7bea9f4790112df11751e0ff2877f4cd4bbde
本尊 <395133701@qq.com>
2017-03-23 21:26:47 +0800

    调试添加微信用户

---
+++ app/Http/Business/WechatBusiness.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Business;
+
+use App\Exceptions\JsonException;
+use App\Http\Controllers\Common\Helper;
+
+class WechatBusiness extends BasicBusiness
+{
+    /**
+     * 关注事件
+     * @param null $openid
+     * @param null $account_info
+     */
+    public function eventSubscribe($openid = null, array $account_info = null)
+    {
+        if (empty($openid) || empty($account_info)) {
+            throw new JsonException(10000);
+        }
+
+        $user_business = app('App\Http\Business\UserBusiness');
+
+        $user_info = $user_business->show($openid, $account_info['admin_user_id'], $account_info['id']);
+
+        // 存在该微信用户
+        if (!empty($user_info)) {
+            $user_business->update($openid, $account_info['admin_user_id'], $account_info['id'], [
+                'subscribe' => 1
+            ]);
+        }
+
+        $wechat = Helper::newWechat([
+            'app_id' => $account_info['app_id'],
+            'secret' => $account_info['secret']
+        ]);
+
+        // 获取微信用户信息
+        $user_info  = $wechat->user->get($openid);
+
+        $user_info['admin_user_id'] = $account_info['admin_user_id'];
+        $user_info['account_id']    = $account_info['id'];
+        \Log::info($user_info);
+        $result  = $user_business->store($user_info);
+        \Log::info($result);
+    }
+}
--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Wechat;
 
 use App\Exceptions\JsonException;
 use App\Http\Business\UserBusiness;
+use App\Http\Business\WechatBusiness;
 use App\Http\Controllers\Common\Helper;
 use App\Http\Business\AccountBusiness;
 use Illuminate\Http\Request;
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Validator;
 
 class WechatController extends Controller
 {
-    public function service(Request $request, AccountBusiness $account_business)
+    public function service(Request $request, AccountBusiness $account_business, WechatBusiness $wechat_business)
     {
         $request_data = $request->only([
             'identity', 'signature', 'timestamp', 'nonce', 'echostr'
@@ -23,32 +24,30 @@ class WechatController extends Controller
         if (empty($request_data['identity']) || strlen($request_data['identity']) != 32) throw new JsonException(1000);
 
         // 获取公众号信息
-        $account_info = $account_business->show($request_data['identity']);
+        $account_info = $account_business->show($request_data['identity'])->toArray();
 
         if (empty($account_info)) throw new JsonException(20001);
 
         $wechat = Helper::newWechat([
-            'app_id'  => $account_info->app_id,
-            'secret'  => $account_info->secret,
-            'token'   => $account_info->token,
-            'aes_key' => $account_info->aes_key
+            'app_id'  => $account_info['app_id'],
+            'secret'  => $account_info['secret'],
+            'token'   => $account_info['token'],
+            'aes_key' => $account_info['aes_key']
         ]);
 
         // 实例化微信服务端
         $wechat_server = $wechat->server;
 
-        $wechat_server->setMessageHandler(function ($message) use ($account_info) {
+        $wechat_server->setMessageHandler(function ($message) use ($account_info, $wechat_business) {
             switch ($message->MsgType) {
                 case 'event':
-                    switch ($message->Event) {
-                        // 关注事件
-                        case 'subscribe':
-                            self::subscribe($message->FromUserName, $account_info);
+                    switch ($message->Event){
+                        case 'subscribe' :
+                            $wechat_business->eventSubscribe($message->FromUserName, $account_info);
                             break;
-                        // 取消关注事件
-                        case 'unsubscribe':
-                            break;
+                        case '' :
                     }
+
                     return '收到事件消息';
                     break;
                 case 'text':
@@ -79,37 +78,11 @@ class WechatController extends Controller
 
         // 微信公众号配置接入请求,并更新微信公众号接入状态
         if (!empty($request_data['echostr']) && $response->getContent() == $request_data['echostr']) {
-            $account_business->update($account_info->identity, [
+            $account_business->update($account_info['identity'], [
                 'activate' => 'yes'
             ]);
         }
 
         return $response;
     }
-
-    /**
-     * 判断opneid是否存在数据库中
-     * @param null $openid
-     * @param Collection $account_info
-     */
-    public function subscribe($openid = null,$account_info, UserBusiness $user_business)
-    {
-        $user_info = $user_business->show($openid, $account_info->admin_user_id, $account_info->id);
-        // 不存在这个微信用户，则获取该微信用户资料，在添加
-        if (empty($user_info)){
-            \Log::info($user_info);
-            $wechat = Helper::newWechat([
-                'app_id' => $account_info->app_id,
-                'secret' => $account_info->secret
-            ]);
-            $user_info = $wechat->user->get($openid);
-            $user_info['admin_user_id'] = $account_info->admin_user_id;
-            $user_info['account_id'] = $account_info->id;
-            \Log::info($user_info);
-            // 添加微信用户
-            $result = $user_business->store($user_info);
-            \Log::info($result);
-        }
-    }
-
 }
